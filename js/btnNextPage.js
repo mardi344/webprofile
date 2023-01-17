773fc14ac127a44d6584fb7bb1a7ed693f71db6d
mardianto <mardi344>
2023-01-18 01:23:15 +0800

    updatePagePertama

---
+++ js/btnNextPage.js
@@ -0,0 +1,5 @@
+const btnNextPage = document.getElementById("btnNextPage");
+
+btnNextPage.addEventListener("click", () => {
+  window.location.href = "/pageportofolio/index.html";
+});
--- js/createtext.js
+++ js/createtext.js
@@ -1,11 +1,12 @@
-var i = 0 ,text;
-text = 'Hello..! welcome to my website please click open portfolio';
+var i = 0,
+  text;
+text = "Hello..! welcome to my website please click open portfolio";
 
 function typing() {
-  if(i < text.length){
-    document.getElementById('text').innerHTML += text.charAt(i);
+  if (i < text.length) {
+    document.getElementById("text").innerHTML += text.charAt(i);
     i++;
-    setTimeout(typing,60);
+    setTimeout(typing, 60);
   }
 }
 
